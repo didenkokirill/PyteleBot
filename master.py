596b78d8d6052fa0c8ec81f49c4b0430daf27119
Didenko Kirill <didenko.kirill11@gmail.com>
2021-11-30 22:39:20 +0300

    version 3 30.11.2021 add big databace need work for them требуемые правки в проекте

--- master.py
+++ master.py
@@ -1,21 +1,27 @@
+import telebot
 import requests
 from bs4 import BeautifulSoup
 
 
+def outInfo(data):
+    for j in data:      #корочь тебе ндо чтоб эта хрень была в формате str чтоб
+        print(data)     #её можно было find и надо както ввытащить из мультисписка
+
+TOKEN = "2078855013:AAFNlYLFXKIBWftwwn3pn8B4lIOwdlQSGtk"
+bot = telebot.TeleBot(token=TOKEN)
+#токен взят у BotFather
+
 url = "https://bitinfocharts.com/ru/crypto-kurs/"
-
-
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
+
+
 print(page.status_code)
-
-
-info_bank = []
+data_bank = []
 with open("currency") as file:
     lines = file.readline()
     for i in range(int(lines)):
         a = file.readline()
-        info_bank.append(soup.findAll("tr", id=f"tr_{int(a)}"))
-print(info_bank)
+        data_bank.append(soup.findAll("tr", id=f"tr_{int(a)}"))
 
-
+print(data_bank)
